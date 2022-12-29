9d685f304c9d5d2b4123d56c20f820b7f2388387
Csandu <ces.pnw@gmail.com>
2022-12-28 20:43:49 -0800

    contact form

---
+++ src/components/Contact.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Element } from "react-scroll";
+import styled from "styled-components";
+import { Container } from "react-bootstrap";
+import { Title } from "./globalStyledComponents";
+import ContactForm from "./ContactForm";
+
+const StyledSection = styled.section`
+  min-height: 88vh;
+  padding-top: var(--nav-height);
+`;
+
+export default function Contact() {
+  return (
+    <Element name={"Contact"} id="contact">
+      <StyledSection className="d-flex flex-column justify-content-center">
+        <Container className="d-flex">
+          <Title>
+            <h2>Contact Me</h2>
+            <div className="underline"></div>
+          </Title>
+        </Container>
+        <Container>
+          <ContactForm />
+        </Container>
+      </StyledSection>
+    </Element>
+  );
+}
