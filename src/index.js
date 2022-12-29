39a1e06bf1ddd170515b256f8c94bee0387676fa
Csandu <ces.pnw@gmail.com>
2022-12-28 18:50:52 -0800

    root

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AppProvider } from "./appContext";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Provider store={store}>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </Provider>
+);
+
+serviceWorkerRegistration.register();
